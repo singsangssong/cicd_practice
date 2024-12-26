67b05cc8c8ff2001f44e1576a901c7f6a283898e
singsangssong <plmko0914@gmail.com>
2024-12-26 14:25:42 +0900

    init all

---
+++ src/main/java/org/example/apple_oauth/AppleOauthApplication.java
@@ -0,0 +1,13 @@
+package org.example.apple_oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppleOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppleOauthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/apple_oauth/AppleOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.apple_oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
